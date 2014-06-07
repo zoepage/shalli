4bf9aaefb38386e4d5adeef6ce42df6801ea4c9b
zoepage <o.gasidlo@gmail.com>
2014-06-07 21:11:51 +0200

    fixed login, missing event listeners

--- www/assets/vendor/hoodie/hoodie.account.js
+++ www/assets/vendor/hoodie/hoodie.account.js
@@ -16,15 +16,16 @@ $(function() {
 // rewirte -> single page application and use it!
       hoodie.account.signIn( $('#usr').val(), $('#pw').val() ).then(
           function(event, done) {
-            window.location.href = 'index.html#/decisions';   
-/*
-            $('.hoodie-account-signedin').fadeIn();   
+
+            $('body').removeClass('signedout').addClass('signedin');
+  
+      //      $('.hoodie-account-signedin').fadeIn();   
             $('.hoodie-accountbar').find('.hoodie-username').text(hoodie.account.username).attr('color', 'red');
-  */          
-            /* -- showing hoodie-accountbar and handling event --- */
+           
+            /* -- showing hoodie-accountbar and handling event --- 
             $('html').attr('data-hoodie-account-status', 'signedin');
             $('.hoodie-accountbar').find('.hoodie-username').text(hoodie.account.user).attr('color', 'red');
-            
+            */
             }, function(event, error) {
         $('.bd').prepend('<div class="alert alert-error">Username or password are wrong!<br /><br /> Please try again!</div>');
       });
