73b03442770e0b6a0a741dce19694ff7d2809710
Hendrik Mittrop <thatsmiddi@gmail.com>
2014-06-07 16:33:21 +0200

    added the routes

--- www/assets/js/router.js
+++ www/assets/js/router.js
@@ -3,3 +3,10 @@
  *
  * @link http://emberjs.com/guides/routing/
  */
+
+ShallI.Router.map(function() {
+	this.resource('decisions', function() {
+		this.resource('new'),
+		this.resource('decisions_id')
+	}),
+});
